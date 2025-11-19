11be354e01552e08d56073bebdc70a8ceded8b3f
itought <mountyjapan24@gmail.com>
2025-11-19 12:54:13 +0200

    Added Stickman

--- Model/Model.py
+++ Model/Model.py
@@ -14,16 +14,71 @@ class Model:
         randomWord = random.choice(self.words)
         hiden = "*" * len(randomWord)
         countLen = len(randomWord)
+        attempts = int(10);
+        mistakes = 0
+
+        Person = \
+        [
+            """
+            
+            
+            """,
+
+            """
+             0
+            """,
+            """
+             0
+             |
+            """,
+            """
+             0
+             |\\
+            """,
+            """
+             0
+            /|\\
+            """,
+            """
+             0
+            /|\\
+            /
+            """,
+            """
+             0
+            /|\\
+            / \\
+            """
+        ]
+
 
         while True:
+            print("\nCurrent word: " + hiden)
             CorrectGuess = ""
             choice = input("Enter you guess: ")
+            attempts -= 1;
+            print("Your current attempts: " + str(attempts))
+
+            if(choice not in randomWord):
+                mistakes += 1
+                if(mistakes >= len(Person)):
+                    print(Person[-1])
+                    print("You got hangmaned, shma")
+                    break
+
+                print (Person[mistakes])
+
             for i in range(len(randomWord)):
                 if randomWord[i] == choice:
                     CorrectGuess += choice
                 else:
                     CorrectGuess += hiden[i]
+
             hiden = CorrectGuess
 
             if(hiden == randomWord):
-                print("Correct!")
+                print("Correct! Word was: " + randomWord)
+                break
+
+user = Model(Model.words)
+user.StartGame()
